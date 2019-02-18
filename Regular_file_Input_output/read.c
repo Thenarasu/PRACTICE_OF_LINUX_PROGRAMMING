fc6e1e1a198804aef8ab33d379a9376fe4d10fce
Thenarasu <tenarasu090493@gmail.com>
2019-02-18 11:44:37 +0530

    Read a regular file and print that

---
+++ Regular_file_Input_output/read.c
@@ -0,0 +1,58 @@
+/*----------------------------------------------------*/
+/*	Author	    : Thennarasu.D		      */
+/*	Date	    : 18Feb2019			      */
+/*	Filename    : read.c			      */
+/*	Description : read a regular_file	      */
+/*----------------------------------------------------*/
+
+
+
+/* include header file  */
+#include "file.h"
+
+/* Global variable declaraations  */
+
+/* Function defenition and prototype  */
+
+int main(int argc,char *argv[])
+{
+	int file_descriptor;
+	ssize_t ret_read;
+	char str1[200] ;
+
+
+
+	if(argc>1)
+	{
+	/* open a file or creates and opens a new file  */
+	/*int open(const char * pathname , int flags , ... mode_t mode)*/
+	file_descriptor = open(argv[1],O_RDONLY,0644);
+	perror("open:");
+
+	/* write a data to an open file  */
+	/* ssize_t write(int fd, void * buffer , size_t count ) */
+	ret_read = read(file_descriptor,str1,25);
+	perror("read");
+	str1[26] = '\0';
+	printf("%s\n",str1);
+	}
+	else 
+	{
+
+	/* open a file or creates and opens a new file  */
+	/*int open(const char * pathname , int flags , ... mode_t mode)*/
+	file_descriptor = open("./ten",O_RDONLY,0644);
+	perror("open");
+
+	/* write a data to an open file  */
+	/* ssize_t write(int fd, void * buffer , size_t count ) */
+	ret_read = read(file_descriptor,str1,25);
+	str1[26] = '\0';
+	perror("read");
+	printf("%s",str1);
+	}
+
+	/* close a file  */
+	close(file_descriptor);
+	perror("close");
+}
---
+++ Regular_file_Input_output/read.h
@@ -0,0 +1,23 @@
+/*----------------------------------------------------*/
+/*	Author	    : Thennarasu.D		      */
+/*	Date	    : 18Feb2019			      */
+/*	Filename    : read.h			      */
+/*	Description : read a regular_file	      */
+/*----------------------------------------------------*/
+
+
+#ifndef	 READ_H
+#define	 READ_H
+
+/* include header files  */
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <error.h>
+#include <string.h>
+#include <unistd.h>
+
+
+/* function prototype  */
+
+#endif 
